c883e54b96f24057e47fdfd0a2a38eea6fefad2c
Ola Bartnik <aleksandra.dadacz@gmail.com>
2020-02-09 22:38:49 +0100

    gulp webpack config

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+const plumber = require("gulp-plumber");
+const c = require("ansi-colors");
+const notifier = require("node-notifier");
+const rename = require("gulp-rename");
+const wait = require("gulp-wait");
+const csso = require("gulp-csso");
+const browserSync = require("browser-sync").create();
+const webpack = require("webpack");
+
+
+const showError = function (err) {
+    //console.log(err.toString());
+
+    notifier.notify({
+        title: "Error in sass",
+        message: err.messageFormatted
+    });
+
+    console.log(c.red("==============================="));
+    console.log(c.red(err.messageFormatted));
+    console.log(c.red("==============================="));
+};
+
+const server = (cb) => {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        },
+        notify: false,
+        //host: "192.168.0.24",
+        //port: 3000,
+        open: true,
+        //browser: "google chrome"
+    });
+
+    cb();
+};
+
+const css = function () {
+    return gulp.src("src/scss/style.scss")
+        .pipe(wait(500))
+        .pipe(plumber({
+            errorHandler: showError
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass({
+                outputStyle: "expanded" //nested, compressed, compact, compressed
+            }).on("error", showError)
+        )
+        .pipe(autoprefixer())
+        .pipe(rename({
+            suffix: ".min",
+            basename: "style"
+        }))
+        .pipe(csso())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream({
+            match: "**/*.css"
+        }));
+};
+
+const js = function (cb) { //https://github.com/webpack/docs/wiki/usage-with-gulp#normal-compilation
+    return webpack(require("./webpack.config.js"), function (err, stats) {
+        if (err) throw err;
+        console.log(stats.toString());
+        browserSync.reload();
+        cb();
+    })
+};
+
+
+
+
+const watch = function () {
+    gulp.watch("src/scss/**/*.scss", {
+        usePolling: true
+    }, gulp.series(css));
+    gulp.watch("src/js/**/*.js", {
+        usePolling: true
+    }, gulp.series(js));
+    gulp.watch("src/html/**/*.html", {
+        usePolling: true
+    }).on("change", browserSync.reload);
+};
+
+
+exports.default = gulp.series(css, js, server, watch);
+exports.css = css;
+exports.watch = watch;
+exports.js = js;
---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import {test, test2} from "./test";
+
+console.log('test')
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    test();
+    test2();
+
+});
